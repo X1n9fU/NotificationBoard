bd9be6388c7852fd97ea8c2db7a545f9ecb26a70
minky <minkyeong02@daum.net>
2024-01-17 23:18:50 +0900

    [INIT] Java 17/ Spring boot 3.1.7/ spring web, lombok, mysql driver, Jpa

---
+++ src/test/java/com/create/NotificationBoard/NotificationBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.create.NotificationBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
